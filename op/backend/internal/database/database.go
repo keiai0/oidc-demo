15d81d7b2da67d86ec8f846b1a12a8a133eda5ce
keito-isurugi <ja224kg@gmail.com>
2026-02-12 21:00:00 +0900

    create database

---
+++ op/backend/internal/database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewDB(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+	return db, nil
+}
---
+++ op/backend/internal/database/migrate.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations(dsn string, migrationsPath string) error {
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		dsn,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create migrate instance: %w", err)
+	}
+	defer m.Close()
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("migration failed: %w", err)
+	}
+	return nil
+}
